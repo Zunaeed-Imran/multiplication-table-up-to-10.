2b4c159faa8e8e5f290357a6a154402ef4070436
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-01 17:50:20 +0600

    multiplication table up to 10, namta in C++

---
+++ print-multiplication.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+  int number;
+
+  cout << "Enter a numner: ";
+  cin >> number;
+
+  cout << "Multiplication is: " << number*1 << " * 1 = " << number*1 << endl;
+  cout << "Multiplication is: " << number*1 << " * 2 = " << number*2 << endl;
+  cout << "Multiplication is: " << number*1 << " * 3 = " << number*3 << endl;
+  cout << "Multiplication is: " << number*1 << " * 4 = " << number*4 << endl;
+  cout << "Multiplication is: " << number*1 << " * 5 = " << number*5 << endl;
+  cout << "Multiplication is: " << number*1 << " * 6 = " << number*6 << endl;
+  cout << "Multiplication is: " << number*1 << " * 7 = " << number*7 << endl;
+  cout << "Multiplication is: " << number*1 << " * 8 = " << number*8 << endl;
+  cout << "Multiplication is: " << number*1 << " * 9 = " << number*9 << endl;
+  cout << "Multiplication is: " << number*1 << " * 10 = " << number*10 << endl;
+
+  return 0;
+}
