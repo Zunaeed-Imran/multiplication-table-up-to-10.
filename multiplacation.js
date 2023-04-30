f3b27e44b7b7c37516dd76ea92d95b083ceee6aa
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-30 14:08:24 +0600

    multiplacation in javaScript
    multiplacation in javaScript (namta)

---
+++ multiplacation.js
@@ -0,0 +1,12 @@
+let num = prompt("Enter number: ");
+
+document.write(num+" * 1 = "+ num * 1 + "<br>");
+document.write(num+" * 2 = "+ num * 2 + "<br>");
+document.write(num+" * 3 = "+ num * 3 + "<br>");
+document.write(num+" * 4 = "+ num * 4 + "<br>");
+document.write(num+" * 5 = "+ num * 5 + "<br>");
+document.write(num+" * 6 = "+ num * 6 + "<br>");
+document.write(num+" * 7 = "+ num * 7 + "<br>");
+document.write(num+" * 8 = "+ num * 8 + "<br>");
+document.write(num+" * 9 = "+ num * 9 + "<br>");
+document.write(num+" * 10 = "+ num * 10 +"<br>");
